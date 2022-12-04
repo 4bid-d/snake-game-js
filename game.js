6bec7c02b56d1d008281d796db4b0f7e5c4f64d3
4bid-d <abidpponline@gmail.com>
2022-12-04 14:16:49 +0530

    finished

---
+++ game.js
@@ -0,0 +1,40 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection } from "./snake.js"
+import { update as updateFood, draw as drawFood } from "./food.js"
+import { outsideGrid } from "./grid.js"
+
+let lastRenderTime = 0
+let gameOver = false 
+const gameBoard = document.getElementById("game-board")
+
+function main(currentTime){
+    if(gameOver){
+        if(confirm("you lose press ok to restart.")){
+            location.href = "/"
+        }
+        return 
+    }
+    const secondsSincelastRender = (currentTime - lastRenderTime) / 1000
+    window.requestAnimationFrame(main)
+    if(secondsSincelastRender < 1 / SNAKE_SPEED) return
+    lastRenderTime = currentTime
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update(){
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
+
+function draw(){
+    gameBoard.innerHTML = ""
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath(){
+    gameOver  =  outsideGrid(getSnakeHead())  || snakeIntersection()
+}
